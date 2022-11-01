6c2700571f92501b682d5794b4ddff67c3eacb8e
benozzy <beno14130@gmail.com>
2022-11-01 13:12:22 -0400

    Fix console history

--- console_ui/console_ui.gd
+++ console_ui/console_ui.gd
@@ -1,7 +1,7 @@
 extends Control
 
-var history: Array[String] = []
-var history_index: int = -1: set = set_history_index
+var history: Array[String] = [""]
+var history_index: int = 0: set = set_history_index
 
 @onready var _scroll_container: ScrollContainer = %ScrollContainer
 @onready var _text_label: RichTextLabel = %TextLabel
@@ -12,6 +12,10 @@ var history_index: int = -1: set = set_history_index
 func _ready() -> void:
 	Console.line_added.connect(_on_line_added)
 	Console.command_executed.connect(_on_command_executed)
+	
+	_line_edit.text_changed.connect(_on_text_changed)
+	_line_edit.text_submitted.connect(_on_text_submitted)
+	
 	Console.register_command(
 		"clear",
 		"Clears the console",
@@ -19,7 +23,6 @@ func _ready() -> void:
 			clear()
 			return OK
 	)
-	_line_edit.text_submitted.connect(_on_text_submitted)
 	
 	open()
 
@@ -36,7 +39,7 @@ func _input(event: InputEvent) -> void:
 		if history_index < history.size() - 1:
 			history_index += 1
 	elif event.is_action_pressed("ui_down"):
-		if history_index > -1:
+		if history_index > 0:
 			history_index -= 1
 
 
@@ -60,11 +63,12 @@ func clear() -> void:
 func set_history_index(value: int) -> void:
 	history_index = value
 	
-	if value != -1:
-		_line_edit.text = history[value]
-		_line_edit.set.call_deferred("caret_column", _line_edit.text.length())
-	else:
-		_line_edit.text = ""
+	_line_edit.text = history[value]
+	_line_edit.set.call_deferred("caret_column", _line_edit.text.length())
+
+
+func _on_text_changed(new_text: String) -> void:
+	history[0] = new_text
 
 
 func _on_text_submitted(new_text: String) -> void:
@@ -79,8 +83,11 @@ func _on_text_submitted(new_text: String) -> void:
 	
 	if history.has(new_text):
 		history.erase(new_text)
-	history.push_front(new_text)
-	history_index = -1
+	history.insert(1, new_text)
+	
+	history[0] = ""
+	history_index = 0
+	
 	_prefix_label.hide()
 	await Console.execute_command(new_text.strip_edges())
 	_prefix_label.show()
